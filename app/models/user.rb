47f75357907cca67b991268bbd5a4d513b83c444
kikumon <s.kiku3728@gmail.com>
2023-10-30 23:53:29 +0900

    before:pr-template

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 #app/models/user.rb
 
 class User < ApplicationRecord
+  validates :name, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatabl#, :confirmable
+         :recoverable, :rememberable, :validatable, :confirmable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
-#app/config/routes.rb
+# app/config/routes.rb
 
 Rails.application.routes.draw do
-  root 'articles#index'
+devise_scope :user do
+  root to: "devise/sessions#new"
+end
+
   get 'articles/index'
-  devise_for :users
-  #resources :articles
+  get '/dashboard', to: 'dashboards#index'
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+  resources :articles
+
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
-
 end
