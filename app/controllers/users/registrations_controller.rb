5cc2014ad68854e4dcab10fc7609a04aa6456deb
kikumon <s.kiku3728@gmail.com>
2023-11-01 02:48:38 +0900

    added:home_controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -59,4 +59,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  protected
+
+  def after_update_path_for(resource)
+    dashboard_path
+  end
+
 end
